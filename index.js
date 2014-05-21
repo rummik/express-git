b20e97a35bf98f739f8519d6964763040b5e0434
rummik <r@9k1.us>
2014-05-21 00:54:10 -0400

    A few small changes, including adding Swig

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 var express = require('express');
 var nodegit = require('nodegit');
+var swig = require('swig');
 
 var app = express();
 
+app.set('view engine', 'html');
+app.engine('html', swig.renderFile);
+
+app.set('views', __dirname + '/views');
+
+if (process.env.NODE_ENV == 'development') {
+	app.set('view cache', false);
+	swig.setDefaults({ cache: false });
+}
+
 app.get('/', function(req, res) {
-	res.send('');
+	res.render('index');
 });
+
+app.use(express.static(__dirname + '/public'));
 
 module.exports = app;
